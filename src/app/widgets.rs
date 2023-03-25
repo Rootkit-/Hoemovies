5ad507d2dc4a2406e29c173f56f216b4dd18a518
Rootkit- <tylerhaup@comcast.net>
2023-03-25 15:23:10 -0500

    ffs

--- src/app/widgets.rs
+++
@@ -1,7 +0,0 @@
-mod game_state;
-mod simulation;
-mod weights;
-
-pub(super) use game_state::GameState;
-pub(super) use simulation::Simulation;
-pub(super) use weights::Weights;
--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,72 @@
-#![forbid(unsafe_code)]
-//#![cfg_attr(not(debug_assertions), deny(warnings))] // Forbid warnings in release builds
-#![warn(clippy::all, rust_2018_idioms)]
+#![feature(const_fn_floating_point_arithmetic)]
 
-mod app;
-pub use app::TemplateApp;
+mod draw;
+mod event;
+mod graphics;
+mod image;
+mod misc;
+mod predefined_scenes;
+mod ui;
+mod vector;
 
-// ----------------------------------------------------------------------------
-// When compiling for web:
+use std::sync::{Arc, Mutex};
 
-#[cfg(target_arch = "wasm32")]
-use eframe::wasm_bindgen::{self, prelude::*};
+use graphics::{CameraInput, SceneList};
+use misc::{update_window_size, RenderState};
+use predefined_scenes::predefined_scenes;
+use ui::Settings;
+use vector::Vec2D;
+use wasm_bindgen::prelude::*;
 
-/// This is the entry-point for all the web-assembly.
-/// This is called once from the HTML.
-/// It loads the app, installs some callbacks, then returns.
-/// You can add more callbacks like this if you want to call in to your code.
-#[cfg(target_arch = "wasm32")]
+// TODO
+// Fix jitter when exiting menus
+// Add some scenes
+// Fix brick ui
+
+// --------------------------------------------------
+
+#[wasm_bindgen(start)]
+pub fn dummy_main() {}
+
 #[wasm_bindgen]
-pub fn start(canvas_id: &str) -> Result<(), eframe::wasm_bindgen::JsValue> {
-    let app = TemplateApp::default();
-    eframe::start_web(canvas_id, Box::new(app))
+pub fn run() {
+    console_error_panic_hook::set_once();
+    console_log::init().expect("error initializing console_log");
+
+    main();
+}
+
+fn main() {
+    let camera_input = Arc::new(Mutex::new(CameraInput::default()));
+    let pointerlock_state = Arc::new(Mutex::new(false));
+    let render_state = Arc::new(Mutex::new(RenderState::StandByMode));
+    let settings = Arc::new(Mutex::new(Settings::default()));
+    let edit_object = Arc::new(Mutex::new(None));
+    let view_size = Arc::new(Mutex::new(Vec2D::new(0u32, 0u32)));
+    let scenes = Arc::new(Mutex::new(SceneList::new(
+        predefined_scenes(),
+        *view_size.lock().unwrap(),
+        settings.lock().unwrap().vfov(),
+    )));
+
+    event::register_ui_events(
+        Arc::clone(&camera_input),
+        Arc::clone(&pointerlock_state),
+        Arc::clone(&render_state),
+        Arc::clone(&scenes),
+        Arc::clone(&settings),
+        Arc::clone(&edit_object),
+        Arc::clone(&view_size),
+    );
+
+    // --------------------------------------------------
+
+    update_window_size(&*settings.lock().unwrap(), &mut *view_size.lock().unwrap());
+    event::render_frame(
+        Arc::clone(&camera_input),
+        Arc::clone(&render_state),
+        Arc::clone(&scenes),
+        Arc::clone(&settings),
+        Arc::clone(&view_size),
+    );
 }
